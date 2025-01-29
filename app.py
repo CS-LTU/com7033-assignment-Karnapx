10f3778a60479b7467ca62a4511f1fc0e09a196f
Ricana Pearson <1901796@leedstrinity.ac.uk>
2025-01-29 23:01:42 +0000

    removed all SQL databases so that I can start again

--- app.py
+++ app.py
@@ -17,60 +17,15 @@ def home():
 def about():
     return render_template('about.html')
 
-# Route for the User Registeration page, now including password encryption
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        users.append({'username': username, 'password': password})
-        return redirect(url_for('home'))
-    return render_template('register.html', fname='Register Here')
-
-#route to handle user registeration
-@app.route('/register', methods=['POST'])
-def do_register():
-    username = request.form['username']
-
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute("INSTERT INTO users (username) VALUES (?)", (username,))
-    conn.commit()
-    conn.close()
-
-    return redirect('/users')
-
-#route for registered users
-@app.route('/users')
-def users():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT username, created_at FROM users")
-    users = cursor.fetchall()
-    conn.close()
-
-    return render_template('users.html', users=users)
-
-if __name__ == '__main__':
-    init_db()
-    app.run(port=5000, debug=False)
+   return render_template ('register.html')
 
 
 # Route for the data page
 @app.route('/data')
 def data():
     return render_template('data.html') 
-
-# linking sql database to backend
-folder_path = r'C:\Users\Rican\OneDrive\Desktop\University🎓\MSc Data Science and AI\SEM 1\COM7033 - Secure Software Development\SSD_Project\com7033-assignment-Karnapx\Databases'
-db_path = os.path.join(folder_path, 'test.db')
-conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM healthcare;")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-conn.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
