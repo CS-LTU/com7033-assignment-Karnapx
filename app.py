d2c71949f183b1916f585680728f593d530e690a
Ricana Pearson <1901796@leedstrinity.ac.uk>
2025-01-30 02:22:54 +0000

    SQL DB inputted for user registration

--- app.py
+++ app.py
@@ -7,22 +7,64 @@ import os
 
 app = Flask(__name__)
 
-#route for the home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Route for the about us page
 @app.route('/aboutus')
 def about():
     return render_template('about.html')
 
+#route for the registration
 @app.route('/register')
 def register():
    return render_template ('register.html')
 
+def init_db():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    ) 
+    ''')
+    conn.commit()
+    conn.close()
 
-# Route for the data page
+
+@app.route('/')
+def register():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def do_register():
+    username = request.form['username']
+
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username) VALUES (?)", (username,))
+    conn.commit()
+    conn.close()
+
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    conn = sqlite3.connect('users.db')
+    cursors = conn.cursor()
+    cursors.execute("SELECT username, created_at FROM users")
+    users = cursors.fetchall()
+    conn.close()
+
+    return render_template('users.html', users=users)
+
+if __name__ == '__main__':
+    init_db()
+    app.run(port=5000, debug=True)
+
+
 @app.route('/data')
 def data():
     return render_template('data.html') 
@@ -35,6 +77,4 @@ def run_flask():
     app.run(host='0.0.0.0', port=5000)
 
 run_flask
-
-
-#check if this needs to be executed in terminal
+ 
